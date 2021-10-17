eafd5cdb90e524c3d5b5382f4a9e71850b8a3d48
pranshu0242 <69257693+pranshu0242@users.noreply.github.com>
2021-10-17 05:37:06 +0530

    Add files via upload

---
+++ multiplayer/js/connect4m.js
@@ -0,0 +1,296 @@
+/**
+ * Minimax Implementation 
+ * @jQuery version
+ */
+ function Game() {
+    this.rows = 6; // Height
+    this.columns = 7; // Width
+    this.status = 0; // 0: running, 1: won, 2: lost, 3: tie
+    this.depth = 4; // Search depth
+    this.score = 100000, // Win/loss score
+    this.round = 0; // 0: Human, 1: Computer
+    this.winning_array = []; // Winning (chips) array
+    this.iterations = 0; // Iteration count
+    
+    that = this;
+
+    that.init();
+}
+
+Game.prototype.init = function() {
+    // Generate 'real' board
+    // Create 2-dimensional array
+    var game_board = new Array(that.rows);
+    for (var i = 0; i < game_board.length; i++) {
+        game_board[i] = new Array(that.columns);
+
+        for (var j = 0; j < game_board[i].length; j++) {
+            game_board[i][j] = null;
+        }
+    }
+
+    // Create from board object (see board.js)
+    this.board = new Board(this, game_board, 0);
+
+    // Generate visual board
+    var game_board = "<col/><col/><col/><col/><col/><col/><col/>";
+    for (var i = 0; i < that.rows; i++) {
+        game_board += "<tr>";
+        for (var j = 0; j < that.columns; j++) {
+            game_board += "<td class='empty'></td>";
+        }
+        game_board += "</tr>";
+    }
+
+    document.getElementById('game_board').innerHTML = game_board;
+
+    // Action listeners
+    var td = document.getElementById('game_board').getElementsByTagName("td");
+
+    for (var i = 0; i < td.length; i++) {
+        if (td[i].addEventListener) {
+            td[i].addEventListener('click', that.act);
+        } else if (td[i].attachEvent) {
+            td[i].attachEvent('click', that.act,false)
+        }
+    }
+}
+
+/**
+ * On-click event
+ */
+Game.prototype.act = function(e) {
+    var element = e.target || window.event.srcElement;
+
+    // Check if not in animation and start with human
+    if (!($('#coin').is(":animated"))) {
+        if (that.round == 0) {
+            that.place(element.cellIndex);
+        }
+    }
+}
+
+/**
+ * Place coin
+ */
+Game.prototype.place = function(column) {
+    // If not finished
+    if (that.board.score() != that.score && that.board.score() != -that.score && !that.board.isFull()) {
+        for (var y = that.rows - 1; y >= 0; y--) {
+            if (document.getElementById('game_board').rows[y].cells[column].className == 'empty') {
+                if (that.round == 1) {
+                    var coin_x = column * 51;
+                    var coin_y = y * 51;
+                    $('#coin').attr('class', 'cpu-coin').css({'left': coin_x}).fadeIn('fast').animate({'top': coin_y + 'px'}, 700, 'easeOutBounce', function() {
+                        document.getElementById('game_board').rows[y].cells[column].className = 'coin cpu-coin';
+                        $('#coin').hide().css({'top': '0px'});
+                        
+                        if (!that.board.place(column)) {
+                            return alert("Invalid move!");
+                        }
+
+                        that.round = that.switchRound(that.round);
+                        that.updateStatus();
+                    });
+                } else {
+                    var coin_x = column * 51;
+                    var coin_y = y * 51;
+                    $('#coin').attr('class', 'human-coin').css({'left': coin_x}).fadeIn('fast').animate({'top': coin_y + 'px'}, 700, 'easeOutBounce', function() {
+                        document.getElementById('game_board').rows[y].cells[column].className = 'coin human-coin';
+                        $('#coin').hide().css({'top': '0px'});
+                        that.generateComputerDecision();
+                        
+                        if (!that.board.place(column)) {
+                            return alert("Invalid move!");
+                        }
+
+                        that.round = that.switchRound(that.round);
+                        that.updateStatus();
+                    });
+                }
+                break;
+            }
+        }
+    }
+}
+
+Game.prototype.generateComputerDecision = function() {
+    // Create from board object (see board.js)
+   
+
+    // Generate visual board
+    
+
+    
+
+    // Action listeners
+    var td = document.getElementById('game_board').getElementsByTagName("td");
+
+    for (var i = 0; i < td.length; i++) {
+        if (td[i].addEventListener) {
+            td[i].addEventListener('click', that.act2);
+        } else if (td[i].attachEvent) {
+            td[i].attachEvent('click', that.act2,false)
+        }
+    }
+}
+Game.prototype.act2 = function(e) {
+    var element = e.target || window.event.srcElement;
+
+    // Check if not in animation and start with human
+    if (!($('#coin').is(":animated"))) {
+        if (that.round == 1) {
+            that.place(element.cellIndex);
+        }
+    }
+}
+
+/**
+ * Algorithm
+ * Minimax principle
+ */
+Game.prototype.maximizePlay = function(board, depth) {
+    // Call score of our board
+    var score = board.score();
+
+    // Break
+    if (board.isFinished(depth, score)) return [null, score];
+
+    // Column, Score
+    var max = [null, -99999];
+
+    // For all possible moves
+    for (var column = 0; column < that.columns; column++) {
+        var new_board = board.copy(); // Create new board
+
+        if (new_board.place(column)) {
+
+            that.iterations++; // Debug
+
+            var next_move = that.minimizePlay(new_board, depth - 1); // Recursive calling
+
+            // Evaluate new move
+            if (max[0] == null || next_move[1] > max[1]) {
+                max[0] = column;
+                max[1] = next_move[1];
+            }
+        }
+    }
+
+    return max;
+}
+
+Game.prototype.minimizePlay = function(board, depth) {
+    var score = board.score();
+
+    if (board.isFinished(depth, score)) return [null, score];
+
+    // Column, score
+    var min = [null, 99999];
+
+    for (var column = 0; column < that.columns; column++) {
+        var new_board = board.copy();
+
+        if (new_board.place(column)) {
+
+            that.iterations++;
+
+            var next_move = that.maximizePlay(new_board, depth - 1);
+
+            if (min[0] == null || next_move[1] < min[1]) {
+                min[0] = column;
+                min[1] = next_move[1];
+            }
+        }
+    }
+    return min;
+}
+
+Game.prototype.switchRound = function(round) {
+    // 0 Human, 1 Computer
+    if (round == 0) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+Game.prototype.updateStatus = function() {
+    // Human won
+    if (that.board.score() == -that.score) {
+        that.status = 1;
+        that.markWin();
+        alert("You have won!");
+    }
+
+    // Computer won
+    if (that.board.score() == that.score) {
+        that.status = 2;
+        that.markWin();
+        alert("You have lost!");
+    }
+
+    // Tie
+    if (that.board.isFull()) {
+        that.status = 3;
+        alert("Tie!");
+    }
+
+    var html = document.getElementById('status');
+    if (that.status == 0) {
+        html.className = "status-running";
+        html.innerHTML = "running";
+    } else if (that.status == 1) {
+        html.className = "status-won";
+        html.innerHTML = "PLayer 1 Won";
+    } else if (that.status == 2) {
+        html.className = "status-lost";
+        html.innerHTML = "Player 2 Won";
+    } else {
+        html.className = "status-tie";
+        html.innerHTML = "tie";
+    }
+}
+
+Game.prototype.markWin = function() {
+    for (var i = 0; i < that.winning_array.length; i++) {
+        var name = document.getElementById('game_board').rows[that.winning_array[i][0]].cells[that.winning_array[i][1]].className;
+        document.getElementById('game_board').rows[that.winning_array[i][0]].cells[that.winning_array[i][1]].className = name + " win";
+    }
+}
+
+Game.prototype.restartGame = function() {
+     var difficulty = document.getElementById('difficulty');
+        var depth = difficulty.options[difficulty.selectedIndex].value;
+        that.depth = depth;
+        that.status = 0;
+        that.round = 0;
+        that.init();
+        document.getElementById('ai-iterations').innerHTML = "?";
+        document.getElementById('ai-time').innerHTML = "?";
+        document.getElementById('ai-column').innerHTML = "Column: ?";
+        document.getElementById('ai-score').innerHTML = "Score: ?";
+        document.getElementById('game_board').className = "";
+        that.updateStatus();
+
+        // Re-assign hover
+        $('td').hover(function() {
+            $(this).parents('table').find('col:eq('+$(this).index()+')').toggleClass('hover');
+        });
+}
+
+/**
+ * Start game
+ */
+function Start() {
+    window.Game = new Game();
+
+    // Hover background, now using jQuery
+    $('td').hover(function() {
+        $(this).parents('table').find('col:eq('+$(this).index()+')').toggleClass('hover');
+    });
+}
+
+window.onload = function() {
+    Start()
+};
